de3aa3b57515294be0d281a6d031288cce8fb24f
Hari19hk <hk19hari@gmail.com>
2026-02-03 23:22:25 +0530

    Fix : change local directory to env for prod

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,19 +1,22 @@
-import admin from 'firebase-admin';
-import fs from 'fs';
+import admin from "firebase-admin";
 
-const serviceAccountPath = '/Users/hk/keys/gdg-service-account.json';
 const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
 
 if (!storageBucket) {
-  throw new Error('FIREBASE_STORAGE_BUCKET not set');
+  throw new Error("FIREBASE_STORAGE_BUCKET not set");
+}
+
+if (!serviceAccountJson) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON not set");
 }
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(serviceAccountJson);
+
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'))
-    ),
-    storageBucket
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket,
   });
 }
 
